3aa41f471d7bf68dab3c941603490374f4273d54
Dzianis Dziarkach <Dzianis_Dziarkach@epam.com>
2018-06-24 02:25:29 +0300

    impemented external bundle

---
+++ DD.AspNet.Web.Optimization.External/ExternalBundle.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Web.Optimization
+{
+    /// <summary>
+    /// External bundle definition.
+    /// Use it bundle for registration files which has been bundled externally via anothed bundler(webpack, gulp, grunt, rollup and etc.).
+    /// </summary>
+    /// <seealso cref="System.Web.Optimization.Bundle" />
+    public sealed class ExternalBundle: Bundle
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExternalBundle"/> class.
+        /// </summary>
+        /// <param name="virtualPath">The virtual path used to reference the <see cref="T:System.Web.Optimization.Bundle" /> from within a view or Web page.</param>
+        /// <param name="cdnPath">An alternate url for the bundle when it is stored in a content delivery network.</param>
+        public ExternalBundle(string virtualPath, string cdnPath = null): base(virtualPath, cdnPath) {}
+    }
+}
---
+++ DDAspNetWebOptimizationExternalTest/ExternalBundleTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Web.Optimization;
+using NUnit.Framework;
+
+namespace DDAspNetWebOptimizationExternalTest
+{
+    [TestFixture]
+    public class ExternalBundleTest
+    {
+        [Test(Description = "Transformations of ExternalBundle should be empty")]
+        public void TransformsListIsEmptyTest()
+        {
+            // Arrange
+            var bundle = new ExternalBundle("~/bundle");
+
+            // Act
+            var hasTransformations = bundle.Transforms.Any();
+
+            // Assert
+            Assert.IsFalse(hasTransformations, "An external bundle includes a transformations");
+        }
+    }
+}
